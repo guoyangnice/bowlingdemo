3f1c0e1f741efed46ca01774b77dd6ad4ac6e426
yangjie <yangjienice@126.com>
2020-07-20 23:35:44 +0800

    需求1完成

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         }
         int score = bowlingGame.getScore();
         System.out.println(score);
-
+        
+        StringBuilder result = bowlingGame.showFramesScore();
+        System.out.println(result);
     }
 }
